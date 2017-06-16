6e3a79385727d1c555681e52c0d3bc726aead397
CocoaHaha <1076800596@qq.com>
2017-06-16 10:21:24 +0800

    Initial Commit

---
+++ StatusBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StatusBar
+//
+//  Created by 石向锋 on 2017/6/16.
+//  Copyright © 2017年 CocoHaHa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
