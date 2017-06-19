b649522fcebd1456a910ab4787b0e0c1c9a5ebba
CocoaHaha <1076800596@qq.com>
2017-06-19 17:35:36 +0800

    额外

---
+++ StatusBar/AAViewController.h
@@ -0,0 +1,13 @@
+//
+//  AAViewController.h
+//  StatusBar
+//
+//  Created by 石向锋 on 2017/6/19.
+//  Copyright © 2017年 CocoHaHa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface AAViewController : ViewController
+
+@end
---
+++ StatusBar/AAViewController.m
@@ -0,0 +1,39 @@
+//
+//  AAViewController.m
+//  StatusBar
+//
+//  Created by 石向锋 on 2017/6/19.
+//  Copyright © 2017年 CocoHaHa. All rights reserved.
+//
+
+#import "AAViewController.h"
+
+@interface AAViewController ()
+
+@end
+
+@implementation AAViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    self.view.backgroundColor =[UIColor blueColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
